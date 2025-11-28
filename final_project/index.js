a2593377d3c72dc184401f5d1f60308760a5655f
ROHIT <2401104114cse@gcekjr.ac.in>
2025-11-29 01:17:40 +0530

    msg

---
+++ final_project/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const session = require('express-session')
+const customer_routes = require('./router/auth_users.js').authenticated;
+const genl_routes = require('./router/general.js').general;
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+
+app.use("/customer/auth/*", function auth(req,res,next){
+  // Check if session has authorization and accessToken
+  if (req.session.authorization) {
+    const token = req.session.authorization.accessToken;
+    try {
+      const decoded = jwt.verify(token, 'access');
+      req.user = decoded;
+      return next();
+    } catch (err) {
+      return res.status(401).json({ message: 'Invalid or expired token' });
+    }
+  }
+  return res.status(401).json({ message: 'User not authenticated' });
+});
+ 
+const PORT =5000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(PORT,()=>console.log("Server is running"));
